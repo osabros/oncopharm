3fd982ae08865224abcfa7df7d8a862e76b9fae9
osabros <osabros@users.noreply.github.com>
2022-11-26 16:05:39 +0200

    packages: added tailwind

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,29 +1,40 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `oncopharm`,
-    siteUrl: `https://www.yourdomain.tld`
+    // description: 'TBD',
+    // image: `/helmet.png`, //https://www.flaticon.com/free-icon/helmet_6186688
+    // socialImage: `/miluimnikim.jpg`,
+    siteUrl: `https://www.yourdomain.tld` // TODO update this
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-postcss', // for tailwind
+    'gatsby-plugin-image',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        'name': 'images',
+        'path': './src/images/'
+      },
+      __key: 'images'
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        'name': 'pages',
+        'path': './src/pages/'
+      },
+      __key: 'pages'
     },
-    __key: "pages"
-  }]
+  ]
 };
 
 export default config;
